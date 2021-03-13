44f8cfb9c7a3b942498ded5f838a82bdd134eb8a
soralee2821 <70058081+soralee2821@users.noreply.github.com>
2021-03-13 19:58:51 +0900

    Add files via upload
    google jQuery를 활용

---
+++ colors2.js
@@ -0,0 +1,26 @@
+ var Body = {
+     setColor:function (color){
+         $('body').css('color',color);
+     },
+    setBackgroundColor:function (color){
+        $('body').css('backgroundColor',color);
+    }
+        }
+var Links = {
+    setColor:function (color){
+    $('a').css('color',color);
+    }
+}
+function nightDayHandler(self){
+    if(self.value == 'night'){
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value = 'day';
+        Links.setColor('powderblue');
+        } else {
+            Body.setBackgroundColor('white');
+            Body.setColor('black');
+            self.value = 'night';
+            Links.setColor('blue');
+        }
+    }
