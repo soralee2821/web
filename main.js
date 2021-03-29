a3492cf79cfbbe58fb00b72de028bb0508799c57
soralee2821 <70058081+soralee2821@users.noreply.github.com>
2021-03-29 21:57:43 +0900

    Add files via upload
    File System의 unlink 를 사용하여 remove 기능 구현하기

--- main.js
+++ main.js
@@ -53,7 +53,13 @@ var app = http.createServer(function(request,response){
 						var title = queryData.id;
 						var list = templateList(filelist);
 						var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, 
-						`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+						`<a href="/create">create</a> 
+						<a href="/update?id=${title}">update</a>
+						<form action="/delete_process" method="post">
+							<input type="hidden" name="id" value="${title}">
+							<input type="submit" value="delete">
+						</form>`
+						);
 						response.writeHead(200);
 						response.end(template);
 					});
@@ -129,6 +135,19 @@ var app = http.createServer(function(request,response){
 				response.end();
 				})
 			});
+		});
+	} else if(pathname === '/delete_process'){
+		var body = ''
+		request.on('data', function(data){
+			body = body + data;
+		})
+		request.on('end', function(){
+			var post = qs.parse(body);
+			var id = post.id;
+			fs.unlink(`data/${id}`, function(error){
+				response.writeHead(302, {Location: `/`});
+				response.end();
+			})
 		});
 	} else {
 		response.writeHead(404);
